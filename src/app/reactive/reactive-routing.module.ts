8ed81371b4eb6029b818bf51b47d9b49dd8d7305
GaOmPS <omarpintado98@gmail.com>
2023-09-20 09:02:51 -0500

    Create routing and forms layouts

---
+++ src/app/reactive/reactive-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {BasicPageComponent} from "./pages/basic-page/basic-page.component";
+import {DynamicPageComponent} from "./pages/dynamic-page/dynamic-page.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basic', component: BasicPageComponent},
+      { path: 'dynamic', component: DynamicPageComponent},
+      { path: 'switches', component: DynamicPageComponent},
+      { path: '**', redirectTo: 'basic'},
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReactiveRoutingModule { }
