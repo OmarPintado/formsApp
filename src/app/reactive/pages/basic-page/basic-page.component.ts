b9a19cb9bd2074d13cd4768ab6addfbca554a014
GaOmPS <omarpintado98@gmail.com>
2023-09-21 07:52:17 -0500

    Create basic form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+/*
+  public myForm:FormGroup = new FormGroup({
+    name : new FormControl('', [],[]),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });
+*/
+
+  constructor( private fb: FormBuilder ) { }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3) ] ],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  isValidField(field:string): boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field:string): string | null {
+    if( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || { };
+
+    for ( const key of Object.keys(errors) ) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return null;
+  }
+
+  onSave():void {
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 
 }
--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BasicPageComponent} from "./pages/basic-page/basic-page.component";
 import {DynamicPageComponent} from "./pages/dynamic-page/dynamic-page.component";
+import {SwitchesPageComponent} from "./pages/switches-page/switches-page.component";
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
     children: [
       { path: 'basic', component: BasicPageComponent},
       { path: 'dynamic', component: DynamicPageComponent},
-      { path: 'switches', component: DynamicPageComponent},
+      { path: 'switches', component: SwitchesPageComponent},
       { path: '**', redirectTo: 'basic'},
     ]
   }
