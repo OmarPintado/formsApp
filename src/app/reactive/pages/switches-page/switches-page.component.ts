e37b1004884e846ec892b09f1eb5cc998ba760d2
GaOmPS <omarpintado98@gmail.com>
2023-09-22 22:17:08 -0500

    Create reactive form

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+});
+
+  constructor( private fb:FormBuilder ) {}
+
+  onSave():void {
+    if (this.myForm.invalid) {
+      this.myForm.markAsTouched();
+      return
+    }
+  }
+
+  isValidField(field:string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
 
 }
