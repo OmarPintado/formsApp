b1bbb867a028fd4394e1f22614044ac3550b2428
GaOmPS <omarpintado98@gmail.com>
2023-09-27 08:28:01 -0500

    Create register form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {cantBeStrider} from "../../../shared/validators/validators";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  constructor (private fb: FormBuilder){ };
+
+  onSubmit():void{
+    console.log(this.myForm);
+    this.myForm.markAllAsTouched();
+  }
 
 }
